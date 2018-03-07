e2d11d8184f0a91b912a8aa6345c61c68353f9a2
Anthony Gawbill <gaw316@gmail.com>
2018-03-07 04:38:53 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function theBeatlesPlay (musicians, instruments) {
+  var array = []
+  for (var i=0; i < 4; i++) {
+  array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return array
+}
+
+function johnLennonFacts(facts) {
+  var array=[]
+  let i=0
+  while (i<facts.length) {
+    array.push(`${facts[i]}!!!`);
+    i++
+  }
+  return array
+}
